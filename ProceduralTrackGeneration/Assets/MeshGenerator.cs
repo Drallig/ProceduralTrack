ed91d885a18ba799d704fe178aed70acf699ab35
DralligStudent <DralligStudent@users.noreply.github.com>
2019-02-19 17:05:38 +0000

    Terrain Generation Continued
    Terrain lowest point will now be at 0 on y axis in world, height clipped at 100(this may need to be altered post track implementation)

--- ProceduralTrackGeneration/Assets/MeshGenerator.cs
+++ ProceduralTrackGeneration/Assets/MeshGenerator.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
-    public int meshSize = 20;
+    public int meshSizeX = 20;
+    public int meshSizeZ = 20;
     public float scale = 1;
 
     Mesh mesh;
@@ -14,6 +15,14 @@ public class MeshGenerator : MonoBehaviour
 
     int[] tris;
 
+    Color[] cols;
+    public Gradient gradient;
+
+    public float bumpFreq;
+
+
+    private float minTer = 100;
+    private float maxTer = 0;
 	// Use this for initialization
 	void Start ()
     {
@@ -34,67 +43,92 @@ public class MeshGenerator : MonoBehaviour
     void CreateShape()
     {
         float ct = Time.deltaTime;
-        verts = new Vector3[(meshSize + 1) * (meshSize + 1)];
+        verts = new Vector3[(meshSizeX + 1) * (meshSizeZ + 1)];
         float r = Random.Range(0.3f,1.7f);
-        if (r > 1.0f)
-        {
-            //scale = scale * 2;
-        }
-
         float r2 = Random.Range(1.5f, 2f);
 
-
-        Debug.Log(r);
-        int i = 0;
-        for (int z = 0; z <= meshSize * 20; z+= 20)
+        /*
+         * ## Loops for construction of terrain mesh, including noise for height of the terrain
+         */
+        for (int z = 0, i = 0; z <= meshSizeZ * 20; z+= 20)
         {
-            for (int x = 0; x <= meshSize * 20; x+= 20)
+            for (int x = 0; x <= meshSizeX * 20; x+= 20)
             {
                 
                 //float y1 = Mathf.PerlinNoise(x * 0.001f, z * 0.001f) * 10f;
                 //float y2 = Mathf.PerlinNoise((float)x / meshSize * scale, (float)z / meshSize * scale);
 
-                float y3 = Mathf.PerlinNoise(x/r * 0.0003f, z/r * 0.0003f) * scale;
-                float y2 = Mathf.PerlinNoise(x / r2 * 0.0003f, z / r2 * 0.0003f);
+                float y3 = Mathf.PerlinNoise(x/r * bumpFreq, z/r * bumpFreq) * scale;
+                float y2 = Mathf.PerlinNoise(x / r2 * bumpFreq, z / r2 * bumpFreq)* scale/3;
                 float y1 = Mathf.PerlinNoise(x * r * 0.000005f, z * r * 0.000005f) * scale;
-                //float y2 = Mathf.PerlinNoise(x / scale * .03f, z / scale * .03f) + offsetY;
-
-                
-
-                //loat ny = ((y1 * y3) + y2) / 3;
-                //float ny = y1 * y2 * y3 / 3;
                 float ny = y2 * y3 / 2;
-                /*if (ny < 0)
-                {
-                    ny = 0.0f;
-                }*/
+
                 verts[i] = new Vector3(x, ny, z);
+                if (ny > maxTer)
+                {
+                    maxTer = ny;
+                }
+                if (ny < minTer)
+                {
+                    minTer = ny;
+                    Debug.Log(minTer);
+                }
                 i++;
             }
         }
 
-        tris = new int[meshSize * meshSize * 6];
+        
+        for (int z = 0, i = 0; z <= meshSizeZ; z++)
+        {
+            for (int x = 0; x <= meshSizeX; x++)
+            {
+                verts[i].y = verts[i].y - minTer;
+                if (verts[i].y > 100)
+                {
+                    verts[i].y = 100;
+                }
+                i++;
+            }
+        }
+
+
+
+        tris = new int[meshSizeX * meshSizeZ * 6];
 
         int cVert = 0;
         int cTris = 0;
 
-        for (int z = 0; z < meshSize; z++)
+        for (int z = 0; z < meshSizeZ; z++)
         {
-            for (int x = 0; x < meshSize; x++)
+            for (int x = 0; x < meshSizeX; x++)
             {
                 tris[cTris + 0] = cVert + 0;
-                tris[cTris + 1] = cVert+ meshSize + 1;
+                tris[cTris + 1] = cVert+ meshSizeZ + 1;
                 tris[cTris + 2] = cVert + 1;
 
                 tris[cTris + 3] = cVert + 1;
-                tris[cTris + 4] = cVert + meshSize + 1;
-                tris[cTris + 5] = cVert + meshSize + 2;
+                tris[cTris + 4] = cVert + meshSizeZ + 1;
+                tris[cTris + 5] = cVert + meshSizeZ + 2;
 
                 cVert++;
                 cTris += 6;
             }
             cVert++;
 
+        }
+
+        cols = new Color[verts.Length];
+
+        for (int i = 0, z = 0; z <= meshSizeZ; z++)
+        {
+            for (int x = 0; x <= meshSizeX; x++)
+            {
+                float h = Mathf.InverseLerp(minTer, maxTer, verts[i].y);
+                cols[i] = gradient.Evaluate(h);
+
+                //uvs[i] = new Vector2((float)x / meshSize, (float)z / meshSize);
+                i++;
+            }
         }
     }
 
@@ -104,6 +138,7 @@ public class MeshGenerator : MonoBehaviour
         mesh.Clear();
         mesh.vertices = verts;
         mesh.triangles = tris;
+        mesh.colors = cols;
 
         mesh.RecalculateNormals();
     }
